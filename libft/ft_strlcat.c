5593cfea71d190c0cc29debd1855039a5a9af0e8
Yoann Danset <ydanset@c1r3p9.42nice.fr>
2021-10-23 16:05:09 +0200

    :x

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydanset <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ydanset <ydanset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:09:32 by ydanset           #+#    #+#             */
-/*   Updated: 2021/10/18 12:55:40 by ydanset          ###   ########.fr       */
+/*   Updated: 2021/10/23 16:00:33 by ydanset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -22,7 +21,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	if (len_dst > dstsize)
 		return (dstsize + ft_strlen(src));
 	i = 0;
-	while ((int)i < (int)dstsize - (int)len_dst - 1
+	while (i < dstsize - len_dst - 1
 		&& src[i] && len_dst + i < dstsize)
 	{
 		dst[len_dst + i] = src[i];
