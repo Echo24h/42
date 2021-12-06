fd70d3ce80a39a942b9c89eddc793e21d769603f
Yoann Danset <ydanset@c1r3p9.42nice.fr>
2021-12-06 09:47:19 +0100

    ;p

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,6 +6,95 @@ int	on_error() // need to track malloc boy
 	return (0);
 }
 
+int	get_index(t_list *lst, t_list *el)
+{
+	int	index;
+
+	index = 0;
+	while (lst)
+	{
+		if (lst->content == el->content)
+			return (index);
+		index++;
+		lst = lst->next;
+	}
+	return (-1);
+}
+
+t_list	*get_max(t_list *lst)
+{
+	t_list	*el_max;
+
+	if (!lst)
+		return (NULL);
+	el_max = lst;
+	lst = lst->next;
+	while (lst)
+	{
+		if (*(int *)lst->content > *(int *)el_max->content)
+			el_max = lst;
+		lst = lst->next;
+	}
+	return (el_max);
+}
+
+t_list	*get_min(t_list *lst)
+{
+	t_list	*el_min;
+
+	if (!lst)
+		return (NULL);
+	el_min = lst;
+	lst = lst->next;
+	while (lst)
+	{
+		if (*(int *)lst->content < *(int *)el_min->content)
+			el_min = lst;
+		lst = lst->next;
+	}
+	return (el_min);
+}
+
+void	move_to_top_a(t_list **a, t_list *el)
+{
+	int	n;
+	int	index;
+
+	n = ft_lstsize(*a);
+	index = get_index(*a, el);
+	if (index < n / 2)
+		while (index--)
+			rotate_a(a);
+	else
+		while (index++ < n)
+			reverse_rotate_a(a);
+}
+
+void	move_to_top_b(t_list **b, t_list *el)
+{
+	int	n;
+	int	index;
+
+	n = ft_lstsize(*b);
+	index = get_index(*b, el);
+	if (index < n / 2)
+		while (index--)
+			rotate_b(b);
+	else
+		while (index++ < n)
+			reverse_rotate_b(b);
+}
+
+void	sorting(t_list **a, t_list **b)
+{
+	t_list	el;
+
+	while (*a)
+	{
+		;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	t_list	*a;
@@ -17,22 +106,8 @@ int	main(int ac, char **av)
 	b = NULL;
 	if (!parse(ac, av, &a))
 		return (on_error());
-	print_stacks(a, b);
-	swap_a(&a);
-	print_stacks(a, b);
-	push_b(&a, &b);
-	print_stacks(a, b);
-	push_b(&a, &b);
-	print_stacks(a, b);
-	swap_b(&b);
-	print_stacks(a, b);
-	push_b(&a, &b);
-	print_stacks(a, b);
-	rotate_ab(&a, &b);
-	print_stacks(a, b);
-	swap_ab(&a, &b);
-	print_stacks(a, b);
-	reverse_rotate_ab(&a, &b);
+	printf("%d\n", get_index(a, get_max(a)));
+	move_to_top_a(&a, get_max(a));
 	print_stacks(a, b);
 	return (0);
 }
