b6e9f34b049c8aa0435b5e3b15973f9d559f4e62
Yoann Danset <ydanset@c1r3p9.42nice.fr>
2022-01-17 17:50:43 +0100

    ;p

---
+++ philosophers/philo/srcs/print_log.c
@@ -0,0 +1,23 @@
+#include "philo.h"
+
+void	print_log(t_philo *ph, long time, int action)
+{
+	long	timestamps;
+	int		n;
+
+	n = ph->id + 1;
+	pthread_mutex_lock(&ph->mu->log);
+	if (!ph->info->stop)
+	{
+		timestamps = time_diff(ph->info->t_start, time);
+		if (action == TAKE_FORK)
+			printf("%ld %d has taken a fork\n", timestamps, n);
+		else if (action == EAT)
+			printf("%ld %d is eating\n", timestamps, n);
+		else if (action == SLEEP)
+			printf("%ld %d is sleeping\n", timestamps, n);
+		else if (action == THINK)
+			printf("%ld %d is thinking\n", timestamps, n);
+	}
+	pthread_mutex_unlock(&ph->mu->log);
+}
