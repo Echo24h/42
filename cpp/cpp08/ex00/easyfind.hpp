3d2ee35a34006b9048058e0d66dd1959a5a81e31
yo-dst <yoann.danset@gmail.com>
2022-06-30 19:26:33 +0200

    :p

--- cpp/cpp08/ex00/easyfind.hpp
+++ cpp/cpp08/ex00/easyfind.hpp
@@ -1,14 +1,7 @@
-#include <algorithm>
-
-/*
-	note that you can do :
-		typedef typename Container::iterator iterator;
-		iterator it;
-*/
-
 template<typename Container>
 int & easyfind(Container ctnr, int toFind) {
-	typename Container::iterator it = std::find(ctnr.begin(), ctnr.end(), toFind);
+	typename Container::iterator it;
+	it = std::find(ctnr.begin(), ctnr.end(), toFind);
 	if (it == ctnr.end()) {
 		throw (std::runtime_error("easyfind: element not found"));
 	} else {
--- cpp/cpp08/ex00/main.cpp
+++ cpp/cpp08/ex00/main.cpp
@@ -9,13 +9,13 @@
 int	main(int ac, char *av[]) {
 	(void)ac;
 	(void)av;
-	std::stack<int> v;
-	v.push(5);
-	v.push(3);
-	v.push(8);
-	v.push(1);
+	std::vector<int> v;
+	v.push_back(5);
+	v.push_back(3);
+	v.push_back(8);
+	v.push_back(1);
 	try {
-		int x = easyfind(v, 8);
+		int x = easyfind(v, 10);
 		std::cout << x << std::endl;
 	} catch (std::runtime_error & e) {
 		std::cout << e.what() << std::endl;
